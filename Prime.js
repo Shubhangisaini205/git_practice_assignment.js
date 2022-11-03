df4ca8e21510f70c6a048f78a5f2bd30aed46c15
sandy shinde <samirshinde011@gmail.com>
2022-11-03 16:06:50 +0530

    correct changes

--- Prime.js
+++ Prime.js
@@ -1,16 +1,18 @@
-function CheckPrime(N){
+function CheckPrime(Nnumber){
  
  let count = 0;
-    for ( let i = 1; i <= N; i++){
-        if ( N % i == 0){
+    for ( let i = 1; i <= Number; i++){
+        if ( Nnumber % i == 0){
             count++;
          }
      }
-    if (count != 2){
-      console.log("Prime");
+    if (count == 2){
+      console.log(number,"Prime");
      }else {
-      console.log("Not Prime");
+      console.log(number,"Not Prime");
      }
 }
 
 CheckPrime(23);
+
+
